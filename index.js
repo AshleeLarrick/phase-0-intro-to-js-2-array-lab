ea37b20e87ae0ea4fbc1d206239af32f70f5c57a
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-01-18 16:39:20 -0700

    Second Time

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+  cats.push(Ralph)
+}
+
+function destructivelyPrependCat(Bob) {
+  cats.unshift(Bob)
+}
+
+function destructivelyRemoveLastCat(Ralph) {
+  cats.pop(Ralph)
+}
+
+function destructivelyRemoveFirstCat(Milo) {
+  cats.shift(Milo)
+}
+
+function appendCat(name) {
+  const copyOfCats = [...cats];
+  copyOfCats.push(name)
+  return copyOfCats
+}
+
+function prependCat(name) {
+  const copyOfCats = [...cats];
+  copyOfCats.unshift(name)
+  return copyOfCats
+}
+
+function removeLastCat() {
+  const copyOfCats = [...cats];
+  copyOfCats.pop(name)
+  return copyOfCats
+}
+
+function removeFirstCat() {
+  const copyOfCats = [...cats];
+  copyOfCats.shift(name)
+  return copyOfCats
+}
